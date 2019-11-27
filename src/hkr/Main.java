3f48b3eba8b5eee66dfb05442f8f1082fac68668
Richsvon <55924965+Richsvon@users.noreply.github.com>
2019-11-27 11:38:08 +0100

    add Tja

--- src/hkr/Main.java
+++ src/hkr/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Hej");
         System.out.println("Hejdå");
+        System.out.println("Tja");
     }
 }
